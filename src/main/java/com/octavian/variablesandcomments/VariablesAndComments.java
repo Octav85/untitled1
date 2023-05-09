12e587d4c0cd4f28bdcc02d58842c5cad52a8523
Octav85 <plopaoctavian@yahoo.com>
2023-05-09 19:08:51 +0300

    Am declarat Variables

---
+++ src/main/java/com/octavian/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,36 @@
+package com.octavian.variablesandcomments;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+
+//        Declararea variabilelor primitive
+
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowerLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+//        Initializarea variabilelor declarate
+
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        firstCharFromAscii = 'a';
+        lastCharFromAscii = 'z';
+
+        System.out.println(firstCharFromAscii);
+
+
+    }
+}

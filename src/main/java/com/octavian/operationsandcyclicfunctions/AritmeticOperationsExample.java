1660dad5abbe82ec1958798ead68a508b94b680f
Octav85 <plopaoctavian@yahoo.com>
2023-05-30 22:06:32 +0300

    Java operation added

---
+++ src/main/java/com/octavian/operationsandcyclicfunctions/AritmeticOperationsExample.java
@@ -0,0 +1,27 @@
+package com.octavian.operationsandcyclicfunctions;
+
+public class AritmeticOperationsExample {
+    public static void main(String[] args) {
+        int a = 42;
+        int c = 3;
+
+        //Adunarea
+        int sum = a + c;
+        System.out.println("Suma este :" + sum);
+        System.out.println("Suma este:" + (a+c));
+
+        //Scaderea
+        int sub = a-c;
+        System.out.println("Rezultatul: " + sub);
+
+        //Multification
+        int result = a * c;
+        System.out.println("inmultirea:" + result);
+
+        //Devision
+         result = a / c;
+        System.out.println("impartirea: " + result);
+
+
+    }
+}
